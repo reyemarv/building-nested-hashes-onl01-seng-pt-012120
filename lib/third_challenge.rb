895691c8c922293a6e066524bd90aae599aa1e66
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 00:03:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,29 +5,29 @@ def third_challenge
         name: "lord Montague", age: "53" 
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
-        name: "Juliet"
-        age: "15"
-        status: "alive"
+        name: "Juliet",
+        age: "15",
+        status: "alive",
         },
       :heroine_friends => []
    }
